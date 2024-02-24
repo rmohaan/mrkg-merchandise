6cc528fb3ef97b1090cc2813b00e28c1c1f1d18b
Mohaan Raja <mohaan.raja@freshworks.com>
2024-02-24 22:26:42 +0530

    Added routing capability and new fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import {Routes, Route} from 'react-router-dom'
 import Categories from './components/categories';
 
 const App = () => {
   return (
     <div className="App">
-      <Categories />
+      <Routes>
+        <Route path='/' element={<Categories />}>
+        </Route>
+      </Routes>
     </div>
   );
 }
